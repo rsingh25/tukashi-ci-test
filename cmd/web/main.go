192d132222a630404c53bc105d001279132b1ccf
Raghvendra <raghvendra.pratap.singh@gmail.com>
2025-08-02 12:45:57 +0530

    sample change

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,8 +11,9 @@ func main() {
 	port := os.Getenv("PORT")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Hello, you've requested:::: %s\n", r.URL.Path)
 	})
 
+	fmt.Printf("Server is starting on port %s...\n", port)
 	http.ListenAndServe(":"+port, nil)
 }
