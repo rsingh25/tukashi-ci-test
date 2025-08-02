698fd8384f9be041273ec7f455f2502bc6e90841
Raghvendra <raghvendra.pratap.singh@gmail.com>
2025-08-02 13:10:25 +0530

    update response message to include hostname

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,9 +9,10 @@ import (
 func main() {
 
 	port := os.Getenv("PORT")
+	host := os.Getenv("HOSTNAME")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested:::: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Hello, this request is served by host %s\n", host)
 	})
 
 	fmt.Printf("Server is starting on port %s...\n", port)
